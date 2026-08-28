be4de5a08fb347ad1e76b8fa36072f4b40126ceb
Rodrigo <estradaleonlarri4@gmail.com>
2026-08-28 12:05:50 -0500

    MAQUETADO DE INTERFAZ DE LOGIN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { NewEvaluation } from './evaluation/pages/new-evaluation/new-evaluation';
 import { ListEvaluation } from './evaluation/pages/list-evaluation/list-evaluation';
+import { LoginPage } from './auth/login/pages/login-page/login-page';
 
 export const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./auth/auth.routes').then(m => m.routes)
+    },
+
+    {
+        path: 'login-page',
+        component: LoginPage
     },
 
     {
@@ -17,4 +23,5 @@ export const routes: Routes = [
         path: 'list-evaluation',
         component: ListEvaluation
     }
+
 ];
