009ed2c83743cda94e843d8d5431c6c404baa5ac
Rodrigo <estradaleonlarri4@gmail.com>
2026-08-28 22:26:22 -0500

    AVANCE FORMULARIO REACTIVO NEW-EVALUATION / FUNCIONALIDAD DE IMC

--- src/app/evaluation/pages/new-evaluation/new-evaluation.ts
+++ src/app/evaluation/pages/new-evaluation/new-evaluation.ts
@@ -1,9 +1,47 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-new-evaluation',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './new-evaluation.html',
   styleUrl: './new-evaluation.css',
 })
-export class NewEvaluation {}
+export class NewEvaluation {
+
+  evaluationForm: FormGroup;
+  imc: number | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.evaluationForm = this.fb.group({
+      peso: [''],
+      altura: [''],
+      dificultadRespirar: [null],
+      tos: [null],
+      silbidoPecho: [null],
+      opresionPecho: [null]
+    });
+  }
+
+  mostrarDatos(): void {
+    console.log(this.evaluationForm.value);
+  }
+
+  calcularIMC(): void {
+    const peso = this.evaluationForm.get('peso')?.value;
+    const alturaCm = this.evaluationForm.get('altura')?.value;
+
+    if (!peso || !alturaCm) {
+      return;
+    }
+
+    const alturaMetros = alturaCm / 100;
+
+    this.imc = Number(
+      (peso / (alturaMetros * alturaMetros)).toFixed(2)
+    );
+  }
+
+  
+
+}
