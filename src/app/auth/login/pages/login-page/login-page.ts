93f666c4c01e6c60db871a36046a8a91ee86da4e
Rodrigo <estradaleonlarri4@gmail.com>
2026-09-05 13:14:50 -0500

    FUNCIONALIDAD DE REGISTRO DE CUENTA PARA USUARIOS

--- src/app/auth/login/pages/login-page/login-page.ts
+++ src/app/auth/login/pages/login-page/login-page.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../services/auth-service';
 import { firstValueFrom } from 'rxjs';
 
 
 @Component({
   selector: 'app-login-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
